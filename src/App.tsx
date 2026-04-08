ebd84ba7bb69faf1b539922754220308bf1b3e9c
Ivan Malinousky <imalinovskiy@turbozaim.ru>
2026-04-08 16:02:03 +0300

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [data, setData] = useState<unknown>({});
   const doLNA = async () => {
     const res = await fetch("http://localhost:3000/posts");
-    console.log(await res.json());
+    setData(await res.json());
   };
 
   return (
     <>
       <button className="counter" onClick={doLNA}>
-        Count is {count}
+        {JSON.stringify(data)}
       </button>
     </>
   );
