d70def8f4dfb825dd0b471ee016571671565bc89
Ivan Malinousky <imalinovskiy@turbozaim.ru>
2026-04-08 16:29:22 +0300

    1

--- src/sw.ts
+++ src/sw.ts
@@ -2,6 +2,10 @@
 
 declare const self: ServiceWorkerGlobalScope;
 
+// @ts-expect-error: __WB_MANIFEST is injected by the build tool
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const manifest = self.__WB_MANIFEST;
+
 // Перехват всех сетевых запросов (fetch и XMLHttpRequest)
 self.addEventListener("fetch", (event) => {
   const url = event.request.url;
